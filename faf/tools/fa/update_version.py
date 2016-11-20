6b52fe250252d1f0b0d82964bf9436c0dd6cc926
IceDreamer <ben-knight@bluebottle.com>
2016-11-20 23:08:51 +0000

    Rework exe version script to allow running from API

--- faf/tools/fa/update_version.py
+++ faf/tools/fa/update_version.py
@@ -1,5 +1,6 @@
 """
-Updates the version in the binary executable of the Forged Alliance game. Will write a new ForgedAlliance.version.exe file.
+Updates the version in the binary executable of the Forged Alliance game. Will write a new ForgedAlliance.version.exe
+file.
 
 Usage:
    update_version <version> [--file=<file>]
@@ -11,10 +12,18 @@ import struct
 import shutil
 from docopt import docopt
 
-if __name__ == '__main__':
-    arguments = docopt(__doc__)
-    file, version = arguments.get('--file'), arguments.get('<version>')
-    shutil.copyfile(file, "ForgedAlliance.%s.exe" % version)
+
+def update_exe_version(source: Path, destination: Path, version: str):
+    """
+    :param source: Path to the static base copy of ForgedAlliance.exe - Hardcoded in API
+    :param destination: Path this update is being copied to
+    :param version: New mod version
+    :return:
+    """
+
+    shutil.copyfile(source, "ForgedAlliance.%s.exe" % version)
+    destination = destination / "ForgedAlliance.%s.exe" % version
+    shutil.move(source, destination)
 
     addr = [0xd3d3f, 0x47612c, 0x476665]
     f = open("ForgedAlliance.%s.exe" % version, 'rb+')
@@ -25,3 +34,9 @@ if __name__ == '__main__':
         f.write(v)
     f.close()
     print("Saved ForgedAlliance.%s.exe".format(version))
+    return f
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    source, destination, version = arguments.get('--file'), arguments.get('destination'), arguments.get('<version>')
+    create_version(source, version)
